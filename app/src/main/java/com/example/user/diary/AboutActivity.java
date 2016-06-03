90df8372d2eb2cd1c780fa4e416cca68a14b5f2c
ViktoriaWofC <vik-tori-s@yandex.ru>
2016-06-03 18:12:39 +0400

    Initial commit

---
+++ app/src/main/java/com/example/user/diary/AboutActivity.java
@@ -0,0 +1,24 @@
+package com.example.user.diary;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+/**
+ * Created by User on 03.06.2016.
+ */
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about_layout);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+}
