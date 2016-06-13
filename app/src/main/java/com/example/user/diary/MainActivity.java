dcac0f2997de6eb7575391a6139fd15dfa7fe56f
ViktoriaWofC <vik-tori-s@yandex.ru>
2016-06-13 13:03:01 +0400

    changes aboutActiviti in aboutFragment

--- app/src/main/java/com/example/user/diary/MainActivity.java
+++ app/src/main/java/com/example/user/diary/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.user.diary;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -26,6 +28,12 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        AboutFragment af = new AboutFragment();
+        ft.replace(R.id.rr, af);
+        ft.commit();
+
     }
 
     @Override
