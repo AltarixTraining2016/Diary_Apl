591b2075f7494ef51f28035db1291796a5af4e8a
ViktoriaWofC <vik-tori-s@yandex.ru>
2016-06-14 01:03:25 +0400

    add navigation menu

--- app/src/main/java/com/example/user/diary/AboutFragment.java
+++ app/src/main/java/com/example/user/diary/AboutFragment.java
@@ -1,16 +1,16 @@
 package com.example.user.diary;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.Menu;
-import android.view.MenuItem;
+        import android.os.Bundle;
+        import android.support.design.widget.FloatingActionButton;
+        import android.support.design.widget.Snackbar;
+        import android.support.v7.app.AppCompatActivity;
+        import android.support.v7.widget.Toolbar;
+        import android.support.v4.app.Fragment;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.view.Menu;
+        import android.view.MenuItem;
 
 
 /**
@@ -25,9 +25,6 @@ public class AboutFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.about_layout, container, false);
-
-        Toolbar toolbar = (Toolbar) v.findViewById(R.id.toolabout);
-        //setSupportActionBar(toolbar);
 
         return v;
     }
--- app/src/main/java/com/example/user/diary/MainActivity.java
+++ app/src/main/java/com/example/user/diary/MainActivity.java
@@ -2,16 +2,20 @@ package com.example.user.diary;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,11 +33,27 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        AboutFragment af = new AboutFragment();
-        ft.replace(R.id.rr, af);
-        ft.commit();
 
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     @Override
@@ -56,5 +76,39 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+
+       // if (drawerToggle.onOptionsItemSelected(item)) {
+       //     return true;
+       // }
+
     }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_main) {
+            //
+        } else if (id == R.id.nav_week) {
+
+        } else if (id == R.id.nav_calendar) {
+
+        } else if (id == R.id.nav_listCase) {
+
+        }else if (id == R.id.nav_about) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            AboutFragment af = new AboutFragment();
+            ft.replace(R.id.container_content, af);
+            ft.commit();
+        }
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
 }
