f87b52ea1e284f795623b1cfeed648bee06fbc4d
ViktoriaWofC <vik-tori-s@yandex.ru>
2016-06-27 16:08:54 +0400

    change case fragment

--- app/src/main/java/com/example/user/diary/CaseFragment.java
+++ app/src/main/java/com/example/user/diary/CaseFragment.java
@@ -7,20 +7,46 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Spinner;
 
 /**
  * Created by User on 08.06.2016.
  */
-public class CaseFragment extends Fragment {
+public class CaseFragment extends Fragment implements TextWatcher{
+
+    String[] stri = new String[] { "Зима", "Весна", "Лето", "Осень" };
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.case_layout, container, false);
 
 
+        AutoCompleteTextView acv = (AutoCompleteTextView)v.findViewById(R.id.autoCompleteTextView);
+        //acv.addTextChangedListener((TextWatcher) getContext());
+        //acv.setAdapter(new ArrayAdapter<String>(getContext(),android.R.layout.simple_dropdown_item_1line, stri));
+
         return v;
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+
     }
 }
